fcb38f9d58f76e24359952147b96c4b6157c651e
Mario Blanco <mario10_bd@hotmail.com>
2024-04-05 16:57:07 +0200

    Colapsar la clase Price y sus derivadas como parte de la responsabilidad de Movie y sus nuevas derivadas

---
+++ src/main/java/usantatecla/movies/v24/Customer.java
@@ -0,0 +1,49 @@
+package usantatecla.movies.v24;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		rentals = new ArrayList<Rental>();
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String statement() {
+		StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
+
+		this.rentals.forEach(rental -> result.append("\t")
+				.append(rental.getMovieTitle())
+				.append("\t")
+				.append(String.valueOf(rental.getCharge()))
+				.append("\n"));
+
+		result.append("Amount owed is ").append(String.valueOf(this.getTotalCharge())).append("\n");
+		result.append("You earned ").append(String.valueOf(this.getTotalFrequentRenterPoints()))
+				.append(" frequent renter points");
+
+		return result.toString();
+	}
+
+	private double getTotalCharge() {
+		return this.rentals.stream().mapToDouble(Rental::getCharge).sum();
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		return this.rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+	}
+
+}
